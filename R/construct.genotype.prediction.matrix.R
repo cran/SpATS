53c0cbbab7e2e7d5c1c5175237ab8593d8801b94
Maria Xose Rodriguez-Alvarez <mxrodriguez@uvigo.es>
2022-11-07 10:40:19 +0000

    version 1.0-18

--- R/construct.genotype.prediction.matrix.R
+++ R/construct.genotype.prediction.matrix.R
@@ -1,9 +1,9 @@
 construct.genotype.prediction.matrix <-
 function(object, newdata) {
-	Z_geno = make_design_matrix(newdata[,object$model$geno$genotype], object$terms$geno$geno_names)
+	Z_geno <- make_design_matrix(newdata[,object$model$geno$genotype], object$terms$geno$geno_names)
 	if(object$model$geno$as.random)
-		Z_geno <- Z_geno[,object$terms$geno$ndx]
+		Z_geno <- Z_geno[,object$terms$geno$ndx, drop = FALSE]
 	else
-		Z_geno <- Z_geno[, object$terms$geno$ndx[2:length(object$terms$geno$ndx)]]
+		Z_geno <- Z_geno[, object$terms$geno$ndx[2:length(object$terms$geno$ndx)], drop = FALSE]
 	Z_geno	
 }
